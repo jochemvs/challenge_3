070ab27b16773b8172ced3c30cf2c163733057f2
Storm Noordermeer <storm.noordermeer@gmail.com>
2022-12-01 09:39:27 +0100

    added clarifying comments

--- get_crypto_data.py
+++ get_crypto_data.py
@@ -1,7 +1,7 @@
 import requests
 import numpy as np
 
-def get_history(crypto, key):
+def get_history(crypto, key): # get all values of the specified crypto up until now
     request_url = f'https://api.basecampcrypto.nl/v1/coin/{crypto}/history?key={key}'
     try:
         r = requests.get(request_url)
@@ -11,11 +11,11 @@ def get_history(crypto, key):
     history = json_return['history']
     dt=np.dtype([('day',int),('value', float)]) # datatypes v/d waarden
     values = [tuple(each.values()) for each in history] # shamelessly gepikt van stackoverflow "https://stackoverflow.com/questions/24792690/easiest-way-to-create-a-numpy-record-array-from-a-list-of-dictionaries"
-    crypto_history_data = np.array(values, dtype=dt)
+    crypto_history_data = np.array(values, dtype=dt) 
     return crypto_history_data
 
 
-def get_current_value(crypto, key):
+def get_current_value(crypto, key): # get current value by coin symbol
     request_url = f'https://api.basecampcrypto.nl/v1/coin/{crypto}?key={key}'
 
     try:
@@ -25,7 +25,7 @@ def get_current_value(crypto, key):
     return r.json()
 
 
-def get_positions(key):
+def get_positions(key): #get positions using team key
     request_url = f'https://api.basecampcrypto.nl/v1/positions?key={key}'
     try:
         r = requests.get(request_url)
@@ -34,7 +34,7 @@ def get_positions(key):
     return r.json()
 
 
-def get_team(key):
+def get_team(key): #get team details using team key
     request_url = f'https://api.basecampcrypto.nl/v1/team?key={key}'
         
     try:
@@ -44,7 +44,7 @@ def get_team(key):
     return r.json()
 
 
-def get_orders(key):
+def get_orders(key): #get orders using team key
     request_url = f'https://api.basecampcrypto.nl/v1/orders?key={key}'
         
     try:
